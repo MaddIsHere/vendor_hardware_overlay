69bc0643e90de39c7b1d0a7afd5d7d09e50a17c9
MaddIsHere <anhktsnamkhanhcs4@gmail.com>
2026-01-21 20:54:38 +0700

    omg help

--- overlay.mk
+++ overlay.mk
@@ -1,6 +1,6 @@
 PRODUCT_PACKAGES += \
 	HardwareOverlayPicker \
 	treble-overlay-samsung-n9 \
+	treble-overlay-samsung-s9 \
 	treble-overlay-samsung-s9p \
 	treble-overlay-samsung-s9p-systemui \
-	treble-overlay-samsung-s9 \
