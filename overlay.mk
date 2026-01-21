30c87b6672f5e34ccddd9358627a5f08394c027a
MaddIsHere <anhktsnamkhanhcs4@gmail.com>
2026-01-21 20:50:23 +0700

    can i get a kiss part 2

--- overlay.mk
+++ overlay.mk
@@ -1,15 +1,5 @@
 PRODUCT_PACKAGES += \
 	HardwareOverlayPicker \
-	QtiAudio \
-	TrebleApp \
-	treble-overlay-NavBar \
-	treble-overlay-NightMode \
-	treble-overlay-SystemUI-FalseLocks \
-	treble-overlay-Telephony-LTE \
-	treble-overlay-caf-ims \
-	treble-overlay-devinputjack \
-	treble-overlay-highpriomisc \
-	treble-overlay-mtk-ims \
 	treble-overlay-samsung-n9 \
 	treble-overlay-samsung-s9p \
 	treble-overlay-samsung-s9p-systemui \
